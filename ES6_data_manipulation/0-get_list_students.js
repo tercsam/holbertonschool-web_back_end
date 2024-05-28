102ad1844d208c1f76b72d13f5d07dee4db16b35
tercsam <mascret.clement@gmail.com>
2024-05-28 09:58:03 -0700

    update js prog

--- ES6_data_manipulation/0-get_list_students.js
+++ ES6_data_manipulation/0-get_list_students.js
@@ -1,7 +1,16 @@
+/**
+ * @function getListStudents
+ * @description Function to get the list of students.
+ * Each student is an object with `id`, `firstName`, and `location` properties.
+ * @returns {Array} Array of student objects.
+ */
 export default function getListStudents() {
     return [
+      // Student object with id 1, firstName 'Guillaume', and location 'San Francisco'
       { id: 1, firstName: 'Guillaume', location: 'San Francisco' },
+      // Student object with id 2, firstName 'James', and location 'Columbia'
       { id: 2, firstName: 'James', location: 'Columbia' },
+      // Student object with id 5, firstName 'Serena', and location 'San Francisco'
       { id: 5, firstName: 'Serena', location: 'San Francisco' },
     ];
-}
+  }

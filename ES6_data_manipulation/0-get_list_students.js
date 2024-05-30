5d0a78bbd61a35bc81080df27169c90313515bbc
tercsam <135607601+tercsam@users.noreply.github.com>
2024-05-30 01:03:34 -0700

    add frist file

--- ES6_data_manipulation/0-get_list_students.js
+++ ES6_data_manipulation/0-get_list_students.js
@@ -1,11 +1,13 @@
-function getListStudents() {
-    return [
-        { id: 1, firstName: 'Guillaume', location: 'San Francisco' },
-        { id: 2, firstName: 'James', location: 'Columbia' },
-        { id: 5, firstName: 'Serena', location: 'San Francisco' }
-    ];
-}
+// Get List Students
 
-// Example usage
-const students = getListStudents();
-console.log(students);
+const getListStudents = () => {
+  const students = [
+    { id: 1, firstName: 'Guillaume', location: 'San Francisco' },
+    { id: 2, firstName: 'James', location: 'Columbia' },
+    { id: 5, firstName: 'Serena', location: 'San Francisco' },
+  ];
+
+  return students;
+};
+
+export default getListStudents;
